2dff82f3245573402156ef3169fb4d9b3535f6d8
fabianelima <fabianelim@gmail.com>
2015-10-26 18:53:28 -0200

    Versão final
    Finalmente cheguei numa versão 1.0. Içasse, fim do projetinho. :)

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 /*--------------- Script --------------*/
 
 /*
-	Com for dá no mesmo. Nem vou fazer.
-
-	Não posso me esquecer da parte de contar os cliques depois. Mas isso
-	vou fazer depois que terminar as ilustrações.
+	YellowDog 1.0
+	Animação corre conforme o clique do usuário. Se não clicar, a animação
+	volta um frame até o final.
 */
 
 var i = 0;
+var j = 0;
 var image = ['anima1.gif','anima2.gif','anima3.gif','anima4.gif','anima5.gif','anima6.gif','anima7.gif','anima8.gif','anima9.gif','anima10.gif','anima11.gif','anima12.gif', 'anima13.gif','anima14.gif','anima15.gif','anima16.gif','anima17.gif','anima18.gif','anima19.gif','anima20.gif','anima21.gif','anima22.gif','anima23.gif','anima24.gif'];
 
 function clique() {
 	i++;
+	j++;
 
 	if (i > 23) {
 		i = 0;
@@ -19,6 +20,7 @@ function clique() {
 
 	$('#image').css('background', 'url(' + image[i] + ') center no-repeat');
 	setTimeout(decay,2500);
+	console.log(j);
 }
 
 function decay() {
