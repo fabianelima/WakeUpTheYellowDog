9811402bd45a14789b8eac7ae4ba2227486230f5
fabianelima <fabianelim@gmail.com>
2015-10-26 14:35:27 -0200

    Imagens
    Doze imagens novas para poder ver se é possível o que quero fazer.

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 /*--------------- Script --------------*/
+
+/*
+	A idéia é fazer um appzinho que funcione com clique do usuário e mude
+	a animação do cursor conforme o usuário vá clicando. Depois do primeiro
+	clique, a animação corre em um sentido, e só muda se o usuário continuar
+	clicando.
+*/
+
 var i = 0;
-var image = ['img1.gif','img2.gif','img3.gif','img4.gif'];
+var image = ['im1.gif','im2.gif','im3.gif','im4.gif','im5.gif','im6.gif','im7.gif','im8.gif','im9.gif','im10.gif','im11.gif','im12.gif'];
 
 $(document).mousemove(function(e){
     $('#image').css({left:e.pageX, top:e.pageY});
@@ -8,7 +16,7 @@ $(document).mousemove(function(e){
 
 function clicou() {
 	i++;
-	if (i > 3) {
+	if (i > 11) {
 		i = 0;
 	}
 	$('#image').css('background', 'url(' + image[i] + ') center no-repeat');
