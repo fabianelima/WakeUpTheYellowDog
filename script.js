369df3132cc1a6edf5401290628e4085324885f1
fabianelima <fabianelim@gmail.com>
2015-10-26 17:10:21 -0200

    Animação
    Agora a animação tem 24 frames.

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 /*--------------- Script --------------*/
 
 /*
-	Consegui fazer o negóz ficar funcional do jeito que eu queria. Agora
-	tenho que deixar o appzinho bonitinho, aproveitar e treinar outras
-	habilidades, usar isso aqui pra brincar com animação frame a frame,
-	quem sabe mostrar na aula... Dá pra fazer um negócio com mais frames.
-	Essa versão atual tem doze.
+	Pronto, fiz uma animaçãozinha com 24 frames. Estava pensando em fazer
+	um 'for' pra não precisar ficar mundando o número no if sempre que
+	ascrescentar mais frames na animação. Acho que vai ser a próxima coisa
+	que vou implementar.
 
 	Não posso me esquecer da parte de contar os cliques depois. Mas isso
 	vou fazer depois que terminar as ilustrações.
 */
 
 var i = 0;
-var image = ['anima1.gif','anima2.gif','anima3.gif','anima4.gif','anima5.gif','anima6.gif','anima7.gif','anima8.gif','anima9.gif','anima10.gif','anima11.gif','anima12.gif'];
+var image = ['anima1.gif','anima2.gif','anima3.gif','anima4.gif','anima5.gif','anima6.gif','anima7.gif','anima8.gif','anima9.gif','anima10.gif','anima11.gif','anima12.gif', 'anima13.gif','anima14.gif','anima15.gif','anima16.gif','anima17.gif','anima18.gif','anima19.gif','anima20.gif','anima21.gif','anima22.gif','anima23.gif','anima24.gif'];
 
 function clique() {
 	i++;
-	if (i > 11) {
+	if (i > 23) {
 		i = 0;
 	}
 	$('#image').css('background', 'url(' + image[i] + ') center no-repeat');
-	setTimeout(decay,2000);
+	setTimeout(decay,3000);
 }
 
 function decay() {
